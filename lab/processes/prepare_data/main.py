005e454e73c6142c590fc01625dd72dc1621df1e
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-02 14:47:16 +0000

    Remove redundant line creating output directory (moved inside prepare_cancer_data)

--- lab/processes/prepare_data/main.py
+++ lab/processes/prepare_data/main.py
@@ -4,11 +4,10 @@ Part 1: Data preparation
 """
 
 import configparser
-from pathlib import Path
 import os
+from pathlib import Path
 
 from processes.prepare_data.cancer_data import prepare_cancer_data
-
 
 PATH_CONFIG = os.getenv("PATH_CONFIG")
 config = configparser.ConfigParser()
@@ -19,5 +18,4 @@ DIR_DATA_PROCESSED = config['paths']['dir_processed']
 
 if __name__ == "__main__":
 
-    Path(DIR_DATA_PROCESSED).mkdir(exist_ok=True)
     prepare_cancer_data(dir_output=DIR_DATA_PROCESSED)
