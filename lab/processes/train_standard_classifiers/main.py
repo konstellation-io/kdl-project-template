05cd721408884982e4ffecc766fa254c0478f5d4
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-03 10:08:06 +0000

    Avoid passing a dictionary to configparser, use as direct function args for train_clkassifiers

--- lab/processes/train_standard_classifiers/main.py
+++ lab/processes/train_standard_classifiers/main.py
@@ -5,6 +5,7 @@ Part 2: Training traditional ML models
 
 import configparser
 import os
+from pathlib import Path
 
 import mlflow
 from processes.train_standard_classifiers.classifiers import train_classifiers
@@ -13,10 +14,10 @@ PATH_CONFIG = os.getenv("PATH_CONFIG")
 config = configparser.ConfigParser()
 config.read(PATH_CONFIG)
 
-config["mlflow"]["mlflow_tags"] = {"git_tag": os.getenv('DRONE_TAG')}
-config["mlflow"]["mlflow_url"] = os.getenv("MLFLOW_URL")
+MLFLOW_URL = os.getenv("MLFLOW_URL")
+MLFLOW_TAGS = {"git_tag": os.getenv('DRONE_TAG')}
 
 
 if __name__ == "__main__":
 
-    train_classifiers(mlflow=mlflow, config=config)
+    train_classifiers(mlflow=mlflow, config=config, mlflow_url=MLFLOW_URL, mlflow_tags=MLFLOW_TAGS)
