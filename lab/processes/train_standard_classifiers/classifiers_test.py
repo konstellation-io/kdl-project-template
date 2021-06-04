0154aba663d91a26f081a8ce93c029e3eb522115
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-04 16:08:38 +0000

    Separate _local.py and _test.py files

--- lab/processes/train_standard_classifiers/classifiers_test.py
+++ lab/processes/train_standard_classifiers/classifiers_test.py
@@ -1,18 +1,22 @@
 """
-Setup for local execution and debugging of scikit-learn classifiers training
+Integration test for step train_standard_classifiers
 """
 
 import configparser
 
+import pytest
 from lib.testing import get_mlflow_stub
-from processes.prepare_data.cancer_data import prepare_cancer_data
 from processes.train_standard_classifiers.classifiers import train_classifiers
 
 vscode_config = configparser.ConfigParser()
 vscode_config.read("lab/processes/config_local.ini")
 
 
-if __name__ == "__main__":
-
-    prepare_cancer_data(dir_output=vscode_config["paths"]["dir_processed"])
+@pytest.mark.integration
+def test_train_classifiers_runs_without_errors(temp_data_dir):
+    """
+    Runs train_classifiers with a mock mlflow instance, failing the test if the run raises any errors
+    Uses test fixture temp_data_dir to create a temporary dataset required by train_classifiers (see conftest.py)
+    """
+    vscode_config['paths']['dir_processed'] = temp_data_dir
     train_classifiers(mlflow=get_mlflow_stub(), config=vscode_config, mlflow_url=None, mlflow_tags=None)
