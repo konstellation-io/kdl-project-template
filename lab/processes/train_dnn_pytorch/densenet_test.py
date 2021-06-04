86470041aab27ab1e59d0296c41a5238e1f13008
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-04 16:20:02 +0000

    Separate _local.py and _test.py files for train_densenet

--- lab/processes/train_dnn_pytorch/densenet_test.py
+++ lab/processes/train_dnn_pytorch/densenet_test.py
@@ -1,18 +1,23 @@
 """
-Setup for local execution and debugging of pytorch densenet training.
+Integration test for process train_dnn_pytorch
 """
 
 import configparser
 
+import pytest
 from lib.testing import get_mlflow_stub
-from processes.prepare_data.cancer_data import prepare_cancer_data
 from processes.train_dnn_pytorch.densenet import train_densenet
 
 vscode_config = configparser.ConfigParser()
 vscode_config.read("lab/processes/config_local.ini")
 
 
-if __name__ == "__main__":
-
-    prepare_cancer_data(dir_output=vscode_config["paths"]["dir_processed"])
+@pytest.mark.integration
+def test_train_densenet_without_errors(temp_data_dir):
+    """
+    Runs train_densenet with a mock mlflow instance and fails the test if the run raises any exceptions
+    Uses test fixture temp_data_dir to create a temporary dataset required by train_densenet (see conftest.py)
+    """
+    vscode_config['paths']['dir_processed'] = temp_data_dir
+    vscode_config['training']['epochs'] = "1"
     train_densenet(mlflow=get_mlflow_stub(), config=vscode_config, mlflow_url=None, mlflow_tags=None)
