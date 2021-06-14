64d7275b9f73b9bd76389bd2e7f13c011a5f4e31
Angel Sanchez Roa <angel.sanchez@intelygenz.com>
2021-06-14 11:40:10 +0200

    Generate private proto code in output process

--- runtimes/scripts/gen_proto.sh
+++ runtimes/scripts/gen_proto.sh
@@ -10,6 +10,7 @@ echo "Public interfaces generated"
 protoc -I=kdl-project-template/processes \
       --python_out=kdl-project-template/processes/etl \
       --python_out=kdl-project-template/processes/model \
+      --python_out=krt/processes/output \
       kdl-project-template/processes/private.proto
 echo "Private interfaces generated"
 
