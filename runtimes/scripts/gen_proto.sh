63552fe1049250330a64f118acc39141faa16c52
vasja.urbancic <vasja.urbancic@intelygenz.com>
2021-06-14 09:50:08 +0000

    Rename dir kdl-project-templates  -> krt

--- runtimes/scripts/gen_proto.sh
+++ runtimes/scripts/gen_proto.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -e
 
-protoc -I=kdl-project-template \
-      --python_out=kdl-project-template/processes/etl \
-      --python_out=kdl-project-template/processes/output \
-      kdl-project-template/public_input.proto
+protoc -I=krt \
+      --python_out=krt/processes/etl \
+      --python_out=krt/processes/output \
+      krt/public_input.proto
 echo "Public interfaces generated"
 
-protoc -I=kdl-project-template/processes \
-      --python_out=kdl-project-template/processes/etl \
-      --python_out=kdl-project-template/processes/model \
+protoc -I=krt/processes \
+      --python_out=krt/processes/etl \
+      --python_out=krt/processes/model \
       --python_out=krt/processes/output \
-      kdl-project-template/processes/private.proto
+      krt/processes/private.proto
 echo "Private interfaces generated"
 
 echo "Done"
