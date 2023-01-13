c109021c24dd6dd00d7487c34608b0a7c23afe15
oauth2 <oauth2@users.noreply.github.com>
2023-01-13 13:01:17 +0000

    added utils function to load params.yaml

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,8 @@
 """
 Miscellaneous utility functions
 """
+import yaml
+from box import ConfigBox
 
 
 def flatten_list(input_list: list) -> list:
@@ -9,3 +11,18 @@ def flatten_list(input_list: list) -> list:
     Only goes one level deep (i.e. works on lists of lists but not lists of lists of lists).
     """
     return [item for sublist in input_list for item in sublist]
+
+
+def load_params(params_path: str) -> ConfigBox:
+    """Load parameters file to be used as object
+
+    Args:
+        params_path (str): route to parameters
+
+    Returns:
+        _type_: _description_
+    """
+    with open(params_path, "r") as f:
+        params = yaml.safe_load(f)
+        params = ConfigBox(params)
+    return params
