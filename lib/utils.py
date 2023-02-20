5a8e6d5dc3da083d172bb136c94eeb66612cbb2c
pablo.fernandez <pablo.fernandez@intelygenz.com>
2023-02-20 11:47:12 +0000

    Modify to use py3nvml

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,9 @@
 """
 Miscellaneous utility functions
 """
-import subprocess
+import time
+
+import py3nvml
 
 
 def flatten_list(input_list: list) -> list:
@@ -12,42 +14,28 @@ def flatten_list(input_list: list) -> list:
     return [item for sublist in input_list for item in sublist]
 
 
-def run_cmd(cmd: str) -> str:
-    """Execute the corresponding cmd
-    and get the output
-    """
-    cmd_out = (subprocess.check_output(cmd, shell=True)).decode("utf-8")[:-1]
-    return cmd_out
+def get_available_gpus_devices(wait: bool = False, refresh_time: int = 10) -> list:
+    """get a list of available gpus
 
-
-def get_available_gpu_devices() -> list:
-    """get available gpus
+    Args:
+        wait (bool, optional): Wait until a gpu is free. Defaults to False.
+        refresh_time (int, optional): how often to recheck if a gpu is available (only when wait=True). Defaults to 10.
 
     Raises:
-        Exception: if no gpu is available
+        Exception: If no gpu is available
 
     Returns:
-        list: indexes of gpus
+        list: device indexes of available gpus
     """
-    # Get a list of existing gpus (their bus name)
-    info_existing_gpu = run_cmd("nvidia-smi -q -d Memory | grep -A4 GPU")
-    info_existing_gpu = (info_existing_gpu.split("\n"))[1:]
-    gpu_bus_ids = [gpu_bus_id.split(" ")[1] for gpu_bus_id in info_existing_gpu if "GPU" in gpu_bus_id]
-    total_gpu_num = len(gpu_bus_ids)
+    state_gpus = py3nvml.get_free_gpus()
+    free_gpus = [index for index, is_free in enumerate(state_gpus) if is_free]
 
-    # Check state of the gpu buses
-    info_gpus_in_use = run_cmd("nvidia-smi --query-compute-apps=gpu_bus_id --format=csv")
-    gpu_bus_ids_in_use = (info_gpus_in_use.split("\n"))[1:]
+    while wait and not free_gpus:
+        time.sleep(refresh_time)
+        state_gpus = py3nvml.get_free_gpus()
+        free_gpus = [index for index, is_free in enumerate(state_gpus) if is_free]
 
-    # Get the gpu index according to their bus name
-    gpu_ids_in_use = []
-    for bus_id in gpu_bus_ids_in_use:
-        gpu_ids_in_use.append(gpu_bus_ids.index(bus_id))
+    if not free_gpus:
+        raise Exception("No available gpus at the moment")
 
-    # Available gpus = gpu index not in used gpus
-    available_gpus = [i for i in range(total_gpu_num) if i not in gpu_ids_in_use]
-
-    if not available_gpus:
-        raise Exception("No available cuda device at the moment")
-
-    return available_gpus
+    return free_gpus
